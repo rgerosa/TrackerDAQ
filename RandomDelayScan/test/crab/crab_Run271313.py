9bcae36679671ba32247597b90e57fbb89dcc9e9
Raffaele <raffaele.gerosa@cern.ch>
2016-04-25 12:58:48 +0200

    random delay scan crab configuration

---
+++ RandomDelayScan/test/crab/crab_Run271313.py
@@ -0,0 +1,29 @@
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+
+config = config()
+
+#### Basic crab config structure
+config.General.requestName  = 'delaySCAN_Express_Run271313'
+config.General.transferLogs = False
+config.General.workArea     = 'crab_trackerDPG'  # Make sure you set this parameter
+
+config.JobType.psetName         = 'trackerdpganalysis_cfg.py'
+config.JobType.pyCfgParams      = 'delayStep=1'
+config.JobType.pluginName       = 'Analysis'
+config.JobType.outputFiles      = ['trackerDPG.root']
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.numCores         = 1
+config.JobType.inputFiles       = ['TI_27-JAN-2010_2_delaystep1.xml','TM_09-JUN-2009_1_delaystep1.xml','TO_30-JUN-2009_1_delaystep1.xml','TP_09-JUN-2009_1_delaystep1.xml','PSUmapping.csv']
+
+config.Data.inputDataset  = '/ExpressPhysics/Run2016A-Express-v2/FEVT' 
+config.Data.inputDBS      = 'global'
+config.Data.splitting     = 'LumiBased'
+config.Data.unitsPerJob   = 3
+config.Data.outLFNDirBase = '/store/user/rgerosa/TrackerDAQ/DELAYSCAN/'
+config.Data.allowNonValidInputDataset = True
+config.Data.publication   = False
+config.Data.lumiMask      = '/afs/cern.ch/user/r/rgerosa/work/TrackerDAQ/DELAY_SCAN/CMSSW_8_0_4/src/DPGAnalysis/SiStripTools/test/crab/json_271313.txt'
+
+config.Site.storageSite = 'T2_CH_CERN'
+
---
+++ RandomDelayScan/test/trackerdpganalysis_cfg.py
@@ -0,0 +1,110 @@
+import FWCore.ParameterSet.Config as cms
+
+### CMSSW command line parameter parser                                                                                                                                          
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('python')
+
+options.register (
+        'delayStep',0,VarParsing.multiplicity.singleton,VarParsing.varType.int,
+        'integer numer to identify the delay xml for each partition');
+
+
+options.parseArguments()
+
+process = cms.Process("clusterAnalysis")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.destinations = ['cout', 'cerr']
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+readFiles = cms.untracked.vstring(options.inputFiles)
+secFiles  = cms.untracked.vstring() 
+process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+
+readFiles.extend(['root://xrootd.unl.edu//store/data/Commissioning2016/MinimumBias/RECO/PromptReco-v1/000/269/214/00000/308C26F4-7601-E611-A809-02163E013789.root'])
+#'/store/express/Commissioning2016/ExpressPhysics/FEVT/Express-v1/000/269/214/00000/0092EB76-CEFF-E511-8D8C-02163E0143AA.root'])
+secFiles.extend([])
+
+# Conditions (Global Tag is used here):
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data_GRun', '')
+#process.GlobalTag.globaltag = '80X_dataRun2_Express_v4'
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True))
+
+#Geometry and field
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+process.load("Geometry.CommonDetUnit.globalTrackingGeometryDB_cfi")
+process.load("TrackingTools.RecoGeometry.RecoGeometries_cff")
+
+process.load("RecoTracker.MeasurementDet.MeasurementTrackerEventProducer_cfi")
+## from Vincenzo Innocente: http://cmslxr.fnal.gov/lxr/source/RecoTracker/TrackProducer/test/TrackRefit.py?v=CMSSW_8_1_0_pre2
+process.load("RecoTracker.TrackProducer.TrackRefitter_cfi")
+process.load('RecoTracker.TrackProducer.TrackRefitters_cff')
+process.TrackRefitter.NavigationSchool = ''
+process.TrackRefitter.Fitter = 'FlexibleKFFittingSmoother'
+process.ttrhbwr.ComputeCoarseLocalPositionFromDisk = True
+process.generalTracksFromRefit = process.TrackRefitter.clone(
+   src = cms.InputTag("generalTracks")
+)
+
+process.doAlldEdXEstimators += process.dedxMedian
+
+process.dedxTruncated40.tracks = "generalTracksFromRefit"
+process.dedxTruncated40.trajectoryTrackAssociation = "generalTracksFromRefit"
+process.dedxHarmonic2.tracks = "generalTracksFromRefit"
+process.dedxHarmonic2.trajectoryTrackAssociation = "generalTracksFromRefit"
+process.dedxMedian.tracks = "generalTracksFromRefit"
+process.dedxMedian.trajectoryTrackAssociation = "generalTracksFromRefit"
+process.dedxHitInfo.tracks = "generalTracksFromRefit"
+process.dedxHitInfo.trajectoryTrackAssociation = "generalTracksFromRefit"
+
+process.refit = cms.Sequence(process.MeasurementTrackerEvent*process.generalTracksFromRefit*process.doAlldEdXEstimators)
+
+process.analysis = cms.EDAnalyzer('TrackerDpgAnalysis',
+   ClustersLabel = cms.InputTag("siStripClusters"),
+   PixelClustersLabel = cms.InputTag("siPixelClusters"),
+   TracksLabel   = cms.VInputTag( cms.InputTag("generalTracksFromRefit")),
+   vertexLabel   = cms.InputTag('offlinePrimaryVertices'),
+   pixelVertexLabel = cms.InputTag('pixelVertices'),
+   beamSpotLabel = cms.InputTag('offlineBeamSpot'),
+   DeDx1Label    = cms.InputTag('dedxHarmonic2'),
+   DeDx2Label    = cms.InputTag('dedxTruncated40'),
+   DeDx3Label    = cms.InputTag('dedxMedian'),
+   L1Label       = cms.InputTag('gtDigis'),
+   HLTLabel      = cms.InputTag("TriggerResults::HLT"),
+   InitalCounter = cms.uint32(1),
+   keepOntrackClusters  = cms.untracked.bool(True),
+   keepOfftrackClusters = cms.untracked.bool(False),
+   keepPixelClusters    = cms.untracked.bool(False),
+   keepPixelVertices    = cms.untracked.bool(False),
+   keepMissingHits      = cms.untracked.bool(False),
+   keepTracks           = cms.untracked.bool(True),
+   keepVertices         = cms.untracked.bool(True),
+   keepEvents           = cms.untracked.bool(True),
+   DelayFileNames = cms.untracked.vstring(
+        "TI_27-JAN-2010_2_delaystep"+str(options.delayStep)+".xml",
+        "TO_30-JUN-2009_1_delaystep"+str(options.delayStep)+".xml",
+        "TP_09-JUN-2009_1_delaystep"+str(options.delayStep)+".xml",
+        "TM_09-JUN-2009_1_delaystep"+str(options.delayStep)+".xml",
+   )
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('trackerDPG.root')
+)
+
+process.skimming = cms.EDFilter("PhysDecl",
+  applyfilter = cms.untracked.bool(False),
+  debugOn = cms.untracked.bool(False),
+  HLTriggerResults = cms.InputTag("TriggerResults","","HLT")
+
+)
+
+process.p = cms.Path(process.skimming*process.refit*process.analysis)
+
+processDumpFile = open('processDump.py', 'w')
+print >> processDumpFile, process.dumpPython()
