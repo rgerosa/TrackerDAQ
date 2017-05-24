6ebb3c295373b9abd965382eebba4bf0f1319542
Raffaele <raffaele.gerosa@cern.ch>
2017-05-24 13:33:43 +0200

    updates

--- RandomDelayScan/test/crab/trackerdpganalysis_cfg.py
+++ RandomDelayScan/test/crab/trackerdpganalysis_cfg.py
@@ -27,6 +27,8 @@ options.register ('isRawFile',False,VarParsing.multiplicity.singleton,VarParsing
 options.register ('isDatFile',False,VarParsing.multiplicity.singleton,VarParsing.varType.bool,
                   'when running as input a dat file file instead of a standard FEVT')
 
+options.register ('dropAnalyzerDumpEDM',False,VarParsing.multiplicity.singleton,VarParsing.varType.bool,
+                  'dump all the collections produced by the config')
 
 options.parseArguments()
 
@@ -37,7 +39,7 @@ from Configuration.StandardSequences.Eras import eras
 process = cms.Process("clusterAnalysis",eras.Run2_2016)
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.destinations = ['cout', 'cerr']
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
 readFiles = cms.untracked.vstring(options.inputFiles)
@@ -72,7 +74,8 @@ process.load("TrackingTools.RecoGeometry.RecoGeometries_cff")
 process.load("RecoTracker.MeasurementDet.MeasurementTrackerEventProducer_cfi")
 ## from Vincenzo Innocente: http://cmslxr.fnal.gov/lxr/source/RecoTracker/TrackProducer/test/TrackRefit.py?v=CMSSW_8_1_0_pre2
 
-## re-fit only when starting from raw-reco of FEVT
+## re-fit only when starting from raw-reco of FEVTi
+process.load('RecoTracker.DeDx.dedxEstimators_cff')
 if not options.isRawFile and not options.isDatFile:
     process.load("RecoTracker.TrackProducer.TrackRefitter_cfi")
     process.load('RecoTracker.TrackProducer.TrackRefitters_cff')
@@ -86,13 +89,9 @@ if not options.isRawFile and not options.isDatFile:
     process.doAlldEdXEstimators += process.dedxMedian
 
     process.dedxTruncated40.tracks = "generalTracksFromRefit"
-    process.dedxTruncated40.trajectoryTrackAssociation = "generalTracksFromRefit"
-    process.dedxHarmonic2.tracks = "generalTracksFromRefit"
-    process.dedxHarmonic2.trajectoryTrackAssociation = "generalTracksFromRefit"
-    process.dedxMedian.tracks = "generalTracksFromRefit"
-    process.dedxMedian.trajectoryTrackAssociation = "generalTracksFromRefit"
-    process.dedxHitInfo.tracks = "generalTracksFromRefit"
-    process.dedxHitInfo.trajectoryTrackAssociation = "generalTracksFromRefit"
+    process.dedxHarmonic2.tracks   = "generalTracksFromRefit"
+    process.dedxMedian.tracks      = "generalTracksFromRefit"
+    process.dedxHitInfo.tracks     = "generalTracksFromRefit"
     process.refit = cms.Sequence(process.MeasurementTrackerEvent*process.generalTracksFromRefit*process.doAlldEdXEstimators)
 
 
@@ -126,9 +125,20 @@ process.analysis = cms.EDAnalyzer('TrackerDpgAnalysis',
    )
 )
 
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string(options.ouputFileName)
-)
+if not options.dropAnalyzerDumpEDM:
+    process.TFileService = cms.Service("TFileService",
+                                       fileName = cms.string(options.ouputFileName)
+                                       )
+else:
+    process.out = cms.OutputModule("PoolOutputModule",    
+                                   fileName = cms.untracked.string(options.ouputFileName),
+                                   outputCommands = cms.untracked.vstring(
+            'drop *',
+            'keep *_*_*_*clusterAnalysis*',
+            
+            ))
+    
+    process.output = cms.EndPath(process.out)
 
 process.skimming = cms.EDFilter("PhysDecl",
   applyfilter = cms.untracked.bool(False),
@@ -140,17 +150,46 @@ process.skimming = cms.EDFilter("PhysDecl",
 if options.isRawFile or options.isDatFile:
     process.load('Configuration.StandardSequences.RawToDigi_cff')    
     process.load('Configuration.StandardSequences.Reconstruction_cff')
-    process.load('RecoTracker.DeDx.dedxEstimators_cff')
     process.analysis.TracksLabel = cms.VInputTag(cms.InputTag("generalTracks"))
-    process.p = cms.Path(process.RawToDigi*
-                         process.reconstruction_trackingOnly* ## local and gloabl reco
-                         process.doAlldEdXEstimators*
-                         process.skimming*
-                         process.dedxMedian*
-                         process.analysis)
+    ## save trajectories everywhere
+    process.generalTracks.copyTrajectories = cms.untracked.bool(True);
+    process.mergedDuplicateTracks.TrajectoryInEvent = cms.bool(True);
+    process.preDuplicateMergingGeneralTracks.copyTrajectories = cms.untracked.bool(True);
+    process.earlyGeneralTracks.copyTrajectories = cms.untracked.bool(True);
+    process.initialStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.initialStepTracksPreSplitting.TrajectoryInEvent = cms.bool(True);
+    process.jetCoreRegionalStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.lowPtTripletStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.pixelPairStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.detachedTripletStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.mixedTripletStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.pixelLessStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.tobTecStepTracks.TrajectoryInEvent = cms.bool(True);
+    process.muonSeededTracksInOut.TrajectoryInEvent = cms.bool(True);
+    process.muonSeededTracksOutIn.TrajectoryInEvent = cms.bool(True);
+
+    if not options.dropAnalyzerDumpEDM:
+        process.p = cms.Path(
+            process.RawToDigi*
+            process.skimming*
+            process.reconstruction_trackingOnly* ## local and gloabl reco
+            process.doAlldEdXEstimators*
+            process.dedxMedian*
+            process.analysis)
+    else:
+        process.p = cms.Path(
+            process.RawToDigi*
+            process.skimming*
+            process.reconstruction_trackingOnly* ## local and gloabl reco
+            process.doAlldEdXEstimators*
+            process.dedxMedian)
+
 
 else:
-    process.p = cms.Path(process.skimming*process.refit*process.analysis)
+    if not options.dropAnalyzerDumpEDM:
+        process.p = cms.Path(process.skimming*process.refit*process.analysis)
+    else:
+        process.p = cms.Path(process.skimming*process.refit)
 
 processDumpFile = open('processDump.py', 'w')
 print >> processDumpFile, process.dumpPython()
